a2f49e553fb054b6fcb3e0b37ce025708cedc76c
Kasib <kasibzxc@gmail.com>
2022-05-21 22:21:31 -0400

    change arrow function back. New stylings for adding notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,27 +4,21 @@ import Footer from "./Footer";
 import Note from "./Note";
 import notes from '../notes';
 
-// function createNote(item) {
-//     return (
-//         <Note 
-//             key={item.key}
-//             name={item.title}
-//             body={item.content}
-//         />
-//     );
-// }
+function createNote(item) {
+    return (
+        <Note 
+            key={item.key}
+            name={item.title}
+            body={item.content}
+        />
+    );
+}
 
 function App() {
     return (
         <div>
             <Header />
-            {notes.map(item => (
-                <Note 
-                    key={item.key}
-                    name={item.title}
-                    body={item.content}
-                />
-            ))}
+            {notes.map(createNote)}
             <Footer />
         </div>
     );
