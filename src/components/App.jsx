adc79c638ca4e0addfbc16786baba8d53434af9f
Kasib <kasibzxc@gmail.com>
2022-05-24 20:54:21 -0400

    keeping track of name and title of entered notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from '../notes';
+import CreateArea from "./CreateArea";
 
 function createNote(item) {
     return (
@@ -18,7 +18,8 @@ function App() {
     return (
         <div>
             <Header />
-            {notes.map(createNote)}
+            <CreateArea />
+            <Note key={1} title='Note title' content='Note content' />
             <Footer />
         </div>
     );
