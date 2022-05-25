e27cf1c47134c75eee9474f58381ef3a5a67f3a8
Kasib <kasibzxc@gmail.com>
2022-05-24 21:20:49 -0400

    ReactDOM depreciated, replaced with createRoot

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,21 +4,27 @@ import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
-function createNote(item) {
-    return (
-        <Note 
-            key={item.key}
-            name={item.title}
-            body={item.content}
-        />
-    );
-}
+// function createNote(item) {
+//     return (
+//         <Note 
+//             key={item.key}
+//             name={item.title}
+//             body={item.content}
+//         />
+//     );
+// }
 
 function App() {
+
+    function addNote(note) {
+        console.log(note);
+    }
+
+    // onAdd created to receive the addNote function
     return (
         <div>
             <Header />
-            <CreateArea />
+            <CreateArea onAdd={addNote}/>
             <Note key={1} title='Note title' content='Note content' />
             <Footer />
         </div>
@@ -26,3 +32,4 @@ function App() {
 }
 
 export default App;
+
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 
-function CreateArea() {
+// trigger Addnote by getting hold of props that gets passed over (props.onAdd)
+function CreateArea(props) {
 
-    // accessed by value in the html
+    // accessed by value in the html - const to keep track of title and content
     const [note, setNote] = useState({
         title: '',
         content: ''
     });
 
-    // use function to handle change
+    // use function to handle change - 
     function handleChange(event) {
         const { name, value } = event.target;
     
-
         setNote(prevNote => {
             return {
                 ...prevNote,
@@ -20,15 +20,25 @@ function CreateArea() {
             };
         })
     }
+
+    // function to pass note back to App - needed as prop as well
+    function submitNote(event) {
+        // prevent refresh
+        event.preventDefault();
+        // pass current created note
+        props.onAdd(note);
+    }
+
     return (
         <div>
             <form>
                 <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
                 <textarea name="content" onChange={handleChange} value={note.content} placeholder="Take a note.." rows="3" />
-                <button>Add</button>
+                <button onClick={submitNote}>Add</button>
             </form>
         </div>
     );
 }
 
 export default CreateArea;
+
